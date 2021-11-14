5c5a02d0858d662e39d5ba2320af4842c960a1a0
chamindu1678 <94241021+chamindu1678@users.noreply.github.com>
2021-11-14 14:48:25 +0530

    Update index.php

--- certificate/index.php
+++ certificate/index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
  <style>
 	 body{ 
-  background: url(76ym.gif) no-repeat center center fixed; 
+  background: url(76YM.gif) no-repeat center center fixed; 
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;

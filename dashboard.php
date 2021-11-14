3b96dfa1798aee125ecbb3fc9d69753a3deddbb9
chamindu1678 <94241021+chamindu1678@users.noreply.github.com>
2021-11-14 13:39:51 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -38,7 +38,7 @@ if(strlen($_SESSION['alogin'])=="")
                     <div class="main-page">
 					<style>
 	 body{ 
-  background: url(76ym.gif) no-repeat center center fixed; 
+  background: url(76YM.gif) no-repeat center center fixed; 
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;

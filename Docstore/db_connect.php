3b31e844b613c2eb62cd3ea315f6d93b8896e5f6
chamindu1678 <94241021+chamindu1678@users.noreply.github.com>
2021-11-14 14:04:35 +0530

    Update db_connect.php

--- Docstore/db_connect.php
+++ Docstore/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','fms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('remotemysql.com','Lvoe7bmmUW','tCIk3xlp4p','Lvoe7bmmUW')or die("Could not connect to mysql".mysqli_error($con));

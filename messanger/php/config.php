b1a5dc0a5987e650cb362f576f73320ebceb7bfa
chamindu1678 <94241021+chamindu1678@users.noreply.github.com>
2021-11-14 14:37:14 +0530

    Update config.php

--- messanger/php/config.php
+++ messanger/php/config.php
@@ -5,7 +5,7 @@
 // 3=> password
 // 4=> database name
 
-$conn = mysqli_connect("localhost","root","","liveChat");
+$conn = mysqli_connect("remotemysql.com","IiK9cRjmk5","GcL0d2ak7o","IiK9cRjmk5");
 
 if(!$conn){
     echo "Connection Failed";

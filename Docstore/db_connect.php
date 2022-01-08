ea5dc12c2960d3e7d10e6f580cc569215624e73d
chamindu1678 <94241021+chamindu1678@users.noreply.github.com>
2022-01-08 13:49:31 +0530

    Update db_connect.php

--- Docstore/db_connect.php
+++ Docstore/db_connect.php
@@ -1,4 +1,4 @@
 <?php 
 
-$conn= new mysqli('db4free.net','apollo_db2','Apollo_db2','apollo_db2')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('db4free.net','apollo_db2','ApolloPlus','apollo_db2')or die("Could not connect to mysql".mysqli_error($con));
 
